dbcf5cfdf9fe0fe908d2e1e46312b27dc1033a6f
lijiabin <2811387295@qq.com>
2020-07-14 17:47:04 +0800

    springcoiud

---
+++ springcloud/parentMavenProject/springcloudEureka/src/main/java/springcloudEureka/EurekaServerApplication.java
@@ -0,0 +1,28 @@
+package springcloudEureka;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import cn.hutool.core.util.NetUtil;
+
+@SpringBootApplication//springboot的程序启动入口
+@EnableEurekaServer//表示这是个 EurekaServer 
+public class EurekaServerApplication {
+	 public static void main(String[] args) {
+	 //8761 这个端口是默认的，就不要修改了，后面的子项目，都会访问这个端口。
+    int port = 8761;
+   
+    if(!NetUtil.isUsableLocalPort(port)) {
+
+        System.err.printf("端口%d被占用了，无法启动%n", port );
+
+        System.exit(1);
+
+    }
+
+    new SpringApplicationBuilder(EurekaServerApplication.class).properties("server.port=" + port).run(args);
+
+}
+}
